8e7c6b68779be6e204a840058d1a12a9ec61d13d
Petyaev Bair <petyaev01@mail.ru>
2022-04-14 09:53:41 +0300

    Mod init/es

--- config/initializers/elasticsearch.rb
+++ config/initializers/elasticsearch.rb
@@ -1,6 +1,4 @@
 require 'elasticsearch/model'
 require 'elasticsearch/transport'
 
-if Rails.env.production?
-	Elasticsearch::Model.client = Elasticsearch::Client.new host: ENV['BONSAI_URL'] || "localhost:9200"
-end
+Elasticsearch::Model.client = Elasticsearch::Client.new host: ENV['BONSAI_URL'] || "localhost:9200"
