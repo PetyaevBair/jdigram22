c9d7add2eee17b40019d901d6e34eb1b8a95077a
Petyaev Bair <petyaev01@mail.ru>
2022-04-25 14:58:08 +0300

    22(4)

--- config/application.rb
+++ config/application.rb
@@ -6,11 +6,12 @@ require "rails/all"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+config.assets.initialize_on_precompile = false
+
 module Jdigram22
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
-    config.assets.initialize_on_precompile = false
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
